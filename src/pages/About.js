833c6be0989503bd3800954ae60d968a04339eac
Victor Souza <victor.augusto800@gmail.com>
2021-10-01 19:08:24 -0300

    Setup about and error page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod quos
+        libero omnis praesentium corporis at nulla fugit eos expedita in
+        asperiores, optio ut? Architecto voluptate quis officia laboriosam.
+        Nostrum, dicta. Similique doloribus laborum qui rerum assumenda
+        voluptatum, tempora voluptas vero!
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
